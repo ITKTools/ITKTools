547557ec7e575fb11d5462f99395b31e5a34f856
stefan <stefan@82fcb030-6448-0410-9922-a3f519602eba>
2006-05-19 12:45:26 +0000

    SK:
    - ADD: Added CommandLineArgumentHelper

---
+++ src/common/CommandLineArgumentHelper.h
@@ -0,0 +1,10 @@
+#ifndef __CommandLineArgumentHelper_h
+#define __CommandLineArgumentHelper_h
+
+#include <string>
+
+void ReplaceUnderscoreWithSpace( std::string & arg );
+
+
+#endif // end #ifndef __CommandLineArgumentHelper_h
+
