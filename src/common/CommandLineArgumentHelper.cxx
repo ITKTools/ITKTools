935892bb8a87f87889dd9a7707f3d0ad2e199f4c
marius <marius@82fcb030-6448-0410-9922-a3f519602eba>
2006-06-09 09:24:47 +0000

    ------------------------------------------------------------ MS:
    BUG: the contents of both fies somehow got switched?
    ------------------------------------------------------------

--- src/common/CommandLineArgumentHelper.cxx
+++ src/common/CommandLineArgumentHelper.cxx
@@ -1,161 +1,18 @@
-#ifndef __itkCommandLineArgumentParser_cxx
-#define __itkCommandLineArgumentParser_cxx
+#ifndef __CommandLineArgumentHelper_cxx
+#define __CommandLineArgumentHelper_cxx
 
-#include "itkCommandLineArgumentParser.h"
+#include "CommandLineArgumentHelper.h"
 
-namespace itk
+void ReplaceUnderscoreWithSpace( std::string & arg )
 {
-
-	/**
-	 * ******************* SetCommandLineArguments *******************
-	 */
-
-	void CommandLineArgumentParser::
-		SetCommandLineArguments( int argc, char **argv )
+	/** Get rid of the possible "_" in arg. */
+	std::basic_string<char>::size_type pos = arg.find( "_" );
+	const std::basic_string<char>::size_type npos = std::basic_string<char>::npos;
+	if ( pos != npos )
 	{
-		m_argv.resize( argc );
-		for ( unsigned int i = 0; i < static_cast<unsigned int>( argc ); i++ )
-		{
-			m_argv[ i ] = argv [ i ];
-		}
-	} // end SetCommandLineArguments
-
-
-	/**
-	 * ******************* FindKey *******************
-	 */
-
-	bool CommandLineArgumentParser::
-		FindKey( const std::string & key, unsigned int & keyIndex, unsigned int & nextKeyIndex )
-		{
-		/** Loop once over the arguments, to get the index of the key,
-		 * and that of the next key. */
-		bool keyFound = false;
-		keyIndex = 0;
-		nextKeyIndex = m_argv.size();
-		for ( unsigned int i = 0; i < m_argv.size(); i++ )
-		{
-			if ( !keyFound && m_argv[ i ] == key )
-			{
-				keyFound = true;
-				keyIndex = i;
-				continue;
-			}
-			if ( keyFound && m_argv[ i ].substr(0,1) == "-" )
-			{
-				if ( !this->IsANumber( m_argv[ i ] ) )
-				{
-					nextKeyIndex = i;
-					break;
-				}
-			}
-		} // end for loop
-
-		/** Check if the key was found and if the next argument is not also a key. */
-		if ( !keyFound ) return false;
-		if ( nextKeyIndex - keyIndex == 1 ) return false;
-
-		return true;
-
-	} // end FindKey()
-
-	/**
-	 * ******************* IsANumber *******************
-	 *
-	 * Checks if something startin with a "-" is a key or a negative number.
-	 */
-
-	bool CommandLineArgumentParser::
-		IsANumber( const std::string & arg )
-		{
-			std::string number = "0123456789";
-			static const std::basic_string<char>::size_type npos = std::basic_string<char>::npos;
-			if ( arg.size() > 1 )
-			{
-				if ( npos != number.find( arg.substr(1,1) ) )
-				{
-					return true;
-				}
-			}
-
-			return false;
-		} // end IsANumber()
-
-	/**
-	 * ******************* GetCommandLineArgument *******************
-	 *
-	 * Different specialisations for String types.
-	 */
-
-	bool CommandLineArgumentParser::
-		GetCommandLineArgument( const std::string & key, std::vector<std::string> & arg )
-	{
-		return this->GetCommandLineArgumentString( key, arg );
+		arg.replace( pos, 1, " " );
 	}
+	
+} // end ReplaceUnderscoreWithSpace
 
-
-	/**
-	 * ******************* GetCommandLineArgument *******************
-	 *
-	 * Different specialisations for Integer types.
-	 */
-
-	bool CommandLineArgumentParser::
-		GetCommandLineArgument( const std::string & key, std::vector<int> & arg )
-	{
-		return this->GetCommandLineArgumentInteger( key, arg );
-	}
-
-	bool CommandLineArgumentParser::
-		GetCommandLineArgument( const std::string & key, std::vector<unsigned int> & arg )
-	{
-		return this->GetCommandLineArgumentInteger( key, arg );
-	}
-
-	bool CommandLineArgumentParser::
-		GetCommandLineArgument( const std::string & key, std::vector<short> & arg )
-	{
-		return this->GetCommandLineArgumentInteger( key, arg );
-	}
-
-	bool CommandLineArgumentParser::
-		GetCommandLineArgument( const std::string & key, std::vector<unsigned short> & arg )
-	{
-		return this->GetCommandLineArgumentInteger( key, arg );
-	}
-
-	bool CommandLineArgumentParser::
-		GetCommandLineArgument( const std::string & key, std::vector<long> & arg )
-	{
-		return this->GetCommandLineArgumentInteger( key, arg );
-	}
-
-	bool CommandLineArgumentParser::
-		GetCommandLineArgument( const std::string & key, std::vector<unsigned long> & arg )
-	{
-		return this->GetCommandLineArgumentInteger( key, arg );
-	}
-
-
-	/**
-	 * ******************* GetCommandLineArgument *******************
-	 *
-	 * Different specialisations for Real types.
-	 */
-
-	bool CommandLineArgumentParser::
-		GetCommandLineArgument( const std::string & key, std::vector<float> & arg )
-	{
-		return this->GetCommandLineArgumentReal( key, arg );
-	}
-
-	bool CommandLineArgumentParser::
-		GetCommandLineArgument( const std::string & key, std::vector<double> & arg )
-	{
-		return this->GetCommandLineArgumentReal( key, arg );
-	}
-
-} // end name space itk
-
-#endif // end #ifndef __itkCommandLineArgumentParser_h
-
+#endif // end #ifndef __CommandLineArgumentHelper_h
