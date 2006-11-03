07401959714d11205b80cb19a1e08b76f6108c89
marius <marius@82fcb030-6448-0410-9922-a3f519602eba>
2006-11-03 12:58:41 +0000

    MS:
    - ENH: PrintHelp()

--- src/getDICOMseriesUIDs/getDICOMseriesUIDs.cxx
+++ src/getDICOMseriesUIDs/getDICOMseriesUIDs.cxx
@@ -15,7 +15,7 @@ int main( int argc, char **argv )
 	if ( argc != 2 || firstarg == "--help" )
 	{
 		std::cout << "Usage:" << std::endl;
-		std::cout << "pxgetseriesUIDs inputDirectoryName" << std::endl;
+		std::cout << "pxgetDICOMseriesUIDs inputDirectoryName" << std::endl;
 		return 1;
 	}
 	std::string inputDirectoryName = argv[ 1 ];
