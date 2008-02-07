c1d99eed5500128515f05c53269c4eb132d06b59
marius <marius@82fcb030-6448-0410-9922-a3f519602eba>
2008-02-07 13:42:03 +0000

    MS:
    - ADD: new program to compute pca of images

---
+++ src/pca/itkPCAImageToImageFilter.h
@@ -0,0 +1,160 @@
+#ifndef _itkPCAImageToImageFilter_h
+#define _itkPCAImageToImageFilter_h
+
+#include "vnl/vnl_vector.h"
+#include "vnl/vnl_matrix.h"
+
+#include "itkImageToImageFilter.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageRegionConstIterator.h"
+
+
+namespace itk
+{
+
+/** \class PCAImageToImageFilter
+ * \brief PCAImageToImageFilter performs a principal component analysis 
+ * (PCA) on a set of images.
+ *
+ * The user specifies the number of input feature images on which a PCA
+ * is performed. Additionally, the number of desired largest principal
+ * components needed can be specified, which is by default equal to the
+ * maximum number of principal components, which is in turn equal to the
+ * number of input feature images.
+ *
+ * The algorithm uses the VNL library to perform the eigen analysis and
+ * to perform some matrix manipulations. This filter gives the same output
+ * as the Matlab function princomp.
+ *
+ * \ingroup ??
+ */ 
+
+template <class TInputImage, 
+          class TOutputImage = Image< double, ::itk::GetImageDimension<TInputImage>::ImageDimension> >
+class ITK_EXPORT PCAImageToImageFilter: 
+    public ImageToImageFilter< TInputImage, TOutputImage >
+{
+public:
+
+  /** Standard class typedefs. */
+  typedef PCAImageToImageFilter             Self;
+  typedef ImageToImageFilter<
+    TInputImage, TOutputImage>              Superclass;
+  typedef SmartPointer<Self>                Pointer;
+  typedef SmartPointer<const Self>          ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro( Self );
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro( PCAImageToImageFilter, ImageToImageFilter );
+
+  /** Typedef's. */
+  typedef TInputImage                               InputImageType;
+  typedef typename InputImageType::Pointer          InputImagePointer;
+  typedef typename InputImageType::ConstPointer     InputImageConstPointer;
+  typedef typename InputImageType::PixelType        InputImagePixelType;
+  typedef TOutputImage                              OutputImageType;
+  typedef typename OutputImageType::PixelType       OutputImagePixelType;
+  typedef typename OutputImageType::Pointer         OutputImagePointer;
+  typedef ImageRegionIterator< TInputImage >        InputImageIterator;
+  typedef ImageRegionConstIterator< TInputImage >   InputImageConstIterator;
+  typedef ImageRegionIterator< TOutputImage >       OutputImageIterator;
+
+  /** Input Image dimension. */
+  itkStaticConstMacro( InputImageDimension, unsigned int, TInputImage::ImageDimension );
+
+  /** Type definition for a vnl double matrix. */
+  typedef vnl_matrix<double> MatrixOfDoubleType; 
+
+  /** Type definition for a vnl double vector. */
+  typedef vnl_vector<double> VectorOfDoubleType;
+
+  /** Set/Get the number of feature images in the input. */
+  virtual void SetNumberOfFeatureImages( unsigned int n );
+  itkGetConstMacro( NumberOfFeatureImages, unsigned int );
+
+  /** Set/Get the number of required largest principal components.
+   * The filter produces the required number of principal components,
+   * but maximally the largest number of principal components.
+   */
+  virtual void SetNumberOfPrincipalComponentsRequired( unsigned int n );
+  itkGetConstMacro( NumberOfPrincipalComponentsRequired, unsigned int );
+
+  /** Get the eigen values. */
+  itkGetConstReferenceMacro( EigenValues, VectorOfDoubleType );
+
+  /** Get the normalised eigen values. */
+  itkGetConstReferenceMacro( NormalisedEigenValues, VectorOfDoubleType );
+
+  /** Get the eigen values. */
+  itkGetConstReferenceMacro( EigenVectors, MatrixOfDoubleType );
+
+protected:
+
+  /** Constructor. */
+  PCAImageToImageFilter();
+
+  /** Destructor. */
+  ~PCAImageToImageFilter(){};
+
+  /** PrintSelf. */
+  virtual void PrintSelf( std::ostream& os, Indent indent ) const;
+
+  /** This filter must produce all of the outputs at once, as such it
+   * must override the EnlargeOutputRequestedRegion method to enlarge the 
+   * output request region.
+   */
+  virtual void EnlargeOutputRequestedRegion( DataObject * );
+
+  /** This filter requires all the input image at once, as such it
+   * must override the GenerateInputRequestedRegion method. Additionally,
+   * this filter assumes that the input images are at least the size as
+   * the first input image.
+   */
+  virtual void GenerateInputRequestedRegion( void );
+
+  /** Starts the image modelling process. */
+  void GenerateData( void );
+
+private:
+
+  PCAImageToImageFilter( const Self& ); // purposely not implemented
+  void operator=( const Self& );        // purposely not implemented
+
+  /** Private functions to control the output. */
+  virtual void SetAndCreateOutputs( unsigned int n );
+  virtual void CheckNumberOfOutputs( void );
+
+  /** Private functions to perform the PCA. */
+  virtual void PerformPCA( void );
+  virtual void CalculateMeanOfFeatureImages( void );
+  virtual void CenterFeatureImages( void );
+  virtual void CalculateCovarianceMatrix( void );
+  virtual void PerformEigenAnalysis( void );
+
+  /** Private variables to store results. */
+  VectorOfDoubleType    m_MeanOfFeatureImages;
+  MatrixOfDoubleType    m_CenteredFeatureImages;
+
+  MatrixOfDoubleType    m_CovarianceMatrix;
+  MatrixOfDoubleType    m_EigenVectors;
+  VectorOfDoubleType    m_EigenValues;
+  VectorOfDoubleType    m_NormalisedEigenValues;
+  MatrixOfDoubleType    m_PrincipalComponents;
+  
+  unsigned int          m_NumberOfPixels;
+  unsigned int          m_NumberOfFeatureImages;
+  unsigned int          m_NumberOfPrincipalComponentsRequired;
+  
+}; // end class PCAImageToImageFilter
+
+
+} // namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkPCAImageToImageFilter.txx"
+#endif
+
+
+#endif // end #ifndef _itkPCAImageToImageFilter_h
---
+++ src/pca/pca.cxx
@@ -0,0 +1,258 @@
+#include "itkCommandLineArgumentParser.h"
+#include "CommandLineArgumentHelper.h"
+
+#include <itksys/SystemTools.hxx>
+#include <sstream>
+#include "itkPCAImageToImageFilter.h"
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+//-------------------------------------------------------------------------------------
+
+/** run: A macro to call a function. */
+#define run(function,type,dim) \
+if ( componentType == #type && Dimension == dim ) \
+{ \
+  typedef itk::Image< type, dim > OutputImageType; \
+  function< OutputImageType >( inputFileNames, outputFileNames, numberOfPCs ); \
+  supported = true; \
+}
+
+//-------------------------------------------------------------------------------------
+
+/** Declare PerformPCA. */
+template< class OutputImageType >
+void PerformPCA(
+  const std::vector< std::string > & inputFileNames,
+  const std::vector< std::string > & outputFileNames,
+  unsigned int numberOfPCs );
+
+/** Declare other functions. */
+void PrintHelp( void );
+
+//-------------------------------------------------------------------------------------
+
+int main( int argc, char **argv )
+{
+	/** Check arguments for help. */
+	if ( argc < 5 || argc > 14 )
+	{
+		PrintHelp();
+		return 1;
+	}
+
+	/** Create a command line argument parser. */
+	itk::CommandLineArgumentParser::Pointer parser = itk::CommandLineArgumentParser::New();
+	parser->SetCommandLineArguments( argc, argv );
+
+	/** Get arguments. */
+  std::vector<std::string>	inputFileNames( 0, "" );
+	bool retin = parser->GetCommandLineArgument( "-in", inputFileNames );
+
+  std::string base = itksys::SystemTools::GetFilenamePath( inputFileNames[ 0 ] );
+  if ( base != "" ) base = base + "/";
+  std::vector<std::string> outputFileNames( inputFileNames.size(), "" );
+  for ( unsigned int i = 0; i < inputFileNames.size(); ++i )
+  {
+    std::ostringstream makeFileName( "" );
+    makeFileName << base << "pc" << i << ".mhd";
+    outputFileNames[ i ] = makeFileName.str();
+  }
+	bool retout = parser->GetCommandLineArgument( "-out", outputFileNames );
+
+  unsigned int numberOfPCs = outputFileNames.size();
+  bool retnpc = parser->GetCommandLineArgument( "-npc", numberOfPCs );
+
+  std::string componentType = "";
+	bool retpt = parser->GetCommandLineArgument( "-opct", componentType );
+
+	/** Check if the required arguments are given. */
+	if ( !retin )
+	{
+		std::cerr << "ERROR: You should specify \"-in\"." << std::endl;
+		return 1;
+	}
+
+  /** Check if enough output file names are specified. */
+  if ( numberOfPCs > outputFileNames.size() )
+  {
+    std::cerr << "ERROR: you should specify " << numberOfPCs << " output file names." << std::endl;
+    return 1;
+  }
+  else
+  {
+    outputFileNames.resize( numberOfPCs );
+  }
+
+  /** Determine image properties. */
+  std::string ComponentTypeIn = "short";
+  std::string	PixelType; //we don't use this
+  unsigned int Dimension = 3;
+  unsigned int NumberOfComponents = 1;
+  std::vector<unsigned int> imagesize( Dimension, 0 );
+  int retgip = GetImageProperties(
+    inputFileNames[ 0 ],
+    PixelType,
+    ComponentTypeIn,
+    Dimension,
+    NumberOfComponents,
+    imagesize );
+  if ( retgip != 0 )
+  {
+    return 1;
+  }
+
+  /** Check for vector images. */
+  if ( NumberOfComponents > 1 )
+  { 
+    std::cerr << "ERROR: The NumberOfComponents is larger than 1!" << std::endl;
+    std::cerr << "Vector images are not supported." << std::endl;
+    return 1; 
+  }
+
+  /** The default output is equal to the input, but can be overridden by   * specifying -pt in the command line.   */  if ( !retpt ) componentType = ComponentTypeIn;
+
+	/** Get rid of the possible "_" in ComponentType. */
+  ReplaceUnderscoreWithSpace( componentType );
+
+	/** Run the program. */
+  bool supported = false;
+	try
+	{
+		run( PerformPCA, unsigned char, 2 );
+		run( PerformPCA, char, 2 );
+		run( PerformPCA, unsigned short, 2 );
+		run( PerformPCA, short, 2 );
+    run( PerformPCA, unsigned int, 2 );
+		run( PerformPCA, int, 2 );
+    run( PerformPCA, unsigned long, 2 );
+		run( PerformPCA, long, 2 );
+    run( PerformPCA, float, 2 );
+		run( PerformPCA, double, 2 );
+
+    run( PerformPCA, unsigned char, 3 );
+		run( PerformPCA, char, 3 );
+		run( PerformPCA, unsigned short, 3 );
+		run( PerformPCA, short, 3 );
+    run( PerformPCA, unsigned int, 3 );
+		run( PerformPCA, int, 3 );
+    run( PerformPCA, unsigned long, 3 );
+		run( PerformPCA, long, 3 );
+    run( PerformPCA, float, 3 );
+		run( PerformPCA, double, 3 );
+	}
+	catch( itk::ExceptionObject &e )
+	{
+		std::cerr << "Caught ITK exception: " << e << std::endl;
+		return 1;
+	}
+  if ( !supported )
+  {
+    std::cerr << "ERROR: this combination of pixeltype and dimension is not supported!" << std::endl;
+    std::cerr <<
+      "pixel (component) type = " << ComponentTypeIn <<
+      " ; dimension = " << Dimension 
+      << std::endl;
+    return 1;
+  }
+	
+	/** End program. */
+	return 0;
+
+} // end main()
+
+
+	/**
+	 * ******************* PerformPCA *******************
+	 */
+
+template< class OutputImageType >
+void PerformPCA(
+  const std::vector< std::string > & inputFileNames,
+  const std::vector< std::string > & outputFileNames,
+  unsigned int numberOfPCs )
+{
+  const unsigned int Dimension = OutputImageType::ImageDimension;
+
+	/** Typedefs. */
+  typedef itk::Image< double, Dimension >               DoubleImageType;
+  typedef itk::PCAImageToImageFilter<
+    DoubleImageType, OutputImageType >                  PCAEstimatorType;
+  typedef typename PCAEstimatorType::VectorOfDoubleType VectorOfDoubleType;
+  typedef typename PCAEstimatorType::MatrixOfDoubleType MatrixOfDoubleType;
+	typedef itk::ImageFileReader< DoubleImageType >			  ReaderType;
+  typedef typename ReaderType::Pointer                  ReaderPointer;
+	typedef itk::ImageFileWriter< OutputImageType >			  WriterType;
+  typedef typename WriterType::Pointer                  WriterPointer;
+
+  /** Get some sizes. */
+  unsigned int noInputs = inputFileNames.size();
+
+  /** Create the PCA estimator. */
+  typename PCAEstimatorType::Pointer pcaEstimator = PCAEstimatorType::New();
+  pcaEstimator->SetNumberOfFeatureImages( noInputs );
+  pcaEstimator->SetNumberOfPrincipalComponentsRequired( numberOfPCs );
+
+  /** For all inputs... */
+  std::vector<ReaderPointer> readers( noInputs );
+  for ( unsigned int i = 0; i < noInputs; ++i )
+  {
+    /** Read in the input images. */
+    readers[ i ] = ReaderType::New();
+    readers[ i ]->SetFileName( inputFileNames[ i ] );
+    readers[ i ]->Update();
+
+    /** Setup PCA estimator. */
+    pcaEstimator->SetInput( i, readers[ i ]->GetOutput() );
+  }
+
+  /** Do the PCA analysis. */
+  pcaEstimator->Update();
+  
+  /** Get eigenvalues and vectors, and print it to screen. */
+  //pcaEstimator->Print( std::cout );
+  VectorOfDoubleType vec = pcaEstimator->GetEigenValues();
+  MatrixOfDoubleType mat = pcaEstimator->GetEigenVectors();
+
+  std::cout << "Eigenvalues: " << std::endl;
+  for ( unsigned int i = 0; i < vec.size(); ++i )
+  {
+    std::cout << vec[ i ] << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Eigenvectors: " << std::endl;
+  for ( unsigned int i = 0; i < vec.size(); ++i )
+  {
+    std::cout << mat.get_row( i ) << std::endl;
+  }
+	
+	/** Setup and process the pipeline. */
+  unsigned int noo = pcaEstimator->GetNumberOfOutputs();
+  std::vector<WriterPointer> writers( noo );
+  for ( unsigned int i = 0; i < noo; ++i )
+  {
+    writers[ i ] = WriterType::New();
+    writers[ i ]->SetFileName( outputFileNames[ i ].c_str() );
+    writers[ i ]->SetInput( pcaEstimator->GetOutput( i ) );
+    writers[ i ]->Update();
+  }
+
+} // end PerformPCA()
+
+
+	/**
+	 * ******************* PrintHelp *******************
+	 */
+
+void PrintHelp( void )
+{
+	std::cout << "Usage:" << std::endl << "pxpca" << std::endl;
+	std::cout << "  -in      inputFilenames" << std::endl;
+	std::cout << "  [-out]   outputFilenames, default pc<i>.mhd" << std::endl;
+  std::cout << "  [-opc]   the number of principal components that you want to output, default all" << std::endl;
+	std::cout << "  [-opct]  output pixel component type, default derived from the input image" << std::endl;
+	std::cout << "Supported: 2D, 3D, (unsigned) char, (unsigned) short, (unsigned) int, (unsigned) long, float, double." << std::endl;
+
+} // end PrintHelp()
