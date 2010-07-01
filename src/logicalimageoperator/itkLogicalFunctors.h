adea3e2f2479d0972fc29b483233405216828d9a
marius.staring <marius.staring@82fcb030-6448-0410-9922-a3f519602eba>
2010-07-01 09:45:04 +0000

    MS:
    -ENH: some code refactoring
    -ENH: added -ops EQUAL

--- src/logicalimageoperator/itkLogicalFunctors.h
+++ src/logicalimageoperator/itkLogicalFunctors.h
@@ -4,28 +4,51 @@
 namespace itk {
 
 namespace Functor {  
-  
-#define itkLogicalFunctorMacro(name,ops)\
-template< class TInput1, class TInput2=TInput1, class TOutput=TInput1 >\
-class name\
-{\
-public:\
-  name() {};\
-  ~name() {};\
-  inline TOutput operator()( const TInput1 & A, const TInput2 & B)\
-  {\
-    return static_cast<TOutput>( ops );\
-  }\
+
+/** Macro for unary logical filter. */
+#define itkLogicalUnaryFunctorMacro( name, ops ) \
+template< class TInput, class TArgument=TInput, class TOutput=TInput > \
+class localName##name \
+{ \
+public: \
+  localName##name() {}; \
+  ~localName##name() {}; \
+  inline TOutput operator()( const TInput & A ) \
+  { \
+    return static_cast<TOutput>( ops ); \
+  } \
+  void SetArgument( TArgument arg ){ this->m_Argument = arg; }; \
+private: \
+  TArgument m_Argument; \
 }
 
-itkLogicalFunctorMacro( AND, A & B );
-itkLogicalFunctorMacro(  OR, A | B );
-itkLogicalFunctorMacro( XOR, A ^ B );
-itkLogicalFunctorMacro( NOT_AND, !(A & B) );
-itkLogicalFunctorMacro( NOT_OR , !(A | B) );
-itkLogicalFunctorMacro( NOT_XOR, !(A ^ B) );
-itkLogicalFunctorMacro( ANDNOT, A & (!B) ); 
-itkLogicalFunctorMacro(  ORNOT, A | (!B) ); 
+/** Macro for binary logical filter. */
+#define itkLogicalBinaryFunctorMacro( name, ops ) \
+template< class TInput1, class TInput2=TInput1, class TOutput=TInput1 > \
+class localName##name \
+{ \
+public: \
+  localName##name() {}; \
+  ~localName##name() {}; \
+  inline TOutput operator()( const TInput1 & A, const TInput2 & B) \
+  { \
+    return static_cast<TOutput>( ops ); \
+  } \
+}
+
+
+/** Create logical filter definitions. */
+itkLogicalUnaryFunctorMacro( NOT,       !A );
+itkLogicalUnaryFunctorMacro( EQUAL,      A == this->m_Argument );
+
+itkLogicalBinaryFunctorMacro( AND,       A & B );
+itkLogicalBinaryFunctorMacro( OR,        A | B );
+itkLogicalBinaryFunctorMacro( XOR,       A ^ B );
+itkLogicalBinaryFunctorMacro( NOT_AND, !(A & B) );
+itkLogicalBinaryFunctorMacro( NOT_OR , !(A | B) );
+itkLogicalBinaryFunctorMacro( NOT_XOR, !(A ^ B) );
+itkLogicalBinaryFunctorMacro( ANDNOT,    A & (!B) ); 
+itkLogicalBinaryFunctorMacro( ORNOT,     A | (!B) ); 
 
 
 } // end namespace Functor
