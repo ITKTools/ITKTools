ca86978f0867ed539e3fa8554e6a2396d808bc72
David Doria <daviddoria@gmail.com>
2011-05-02 18:41:11 -0400

    ENH: Test structure

--- src/CompareScript.cmake
+++
@@ -1,3 +0,0 @@
-#This script is necessary to run two commands in the same test. We need to do this because the first command will be the executable that produces an output, and the second command will be running the comparison to the baseline output.
-execute_process(COMMAND ${command1})
-execute_process(COMMAND ${command2})
