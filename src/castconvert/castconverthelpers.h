5005bce9cc955e863d3bfc2293c787c34ddb77a4
Marius Staring <m.staring@lumc.nl>
2011-05-18 16:48:41 +0200

    ENH: added DICOM support again to pxcastconvert COMP: broke pxcastconvert into smaller compilation units ENH: added -opct support again to pxcastconvert ENH: some ITKToolsHelper function signatures made by reference

--- src/castconvert/castconverthelpers.h
+++ src/castconvert/castconverthelpers.h
@@ -18,9 +18,14 @@
 #ifndef __castconverthelpers_h__
 #define __castconverthelpers_h__
 
+#include "ITKToolsBase.h"
+#include "ITKToolsHelpers.h"
+#include <iostream>
+
 /** Basic Image support. */
-#include "itkImage.h"
 #include "itkImageIORegion.h"
+#include "itkImage.h"
+#include "itkVectorImage.h"
 
 /** Reading and writing images. */
 #include "itkImageFileReader.h"
@@ -32,9 +37,214 @@
 #include "itkGDCMSeriesFileNames.h"
 
 /** One of these is used to cast the image. */
-#include "itkShiftScaleImageFilter.h"
-//#include "itkRescaleIntensityImageFilter.h"
+#include "itkCastImageFilter.h"
 #include "itkVectorCastImageFilter.h"
+#include "itkVectorIndexSelectionCastImageFilter.h"
+#include "itkImageToVectorImageFilter.h"
+#include "itkShiftScaleImageFilter.h"
+
+
+/**
+ * \class ITKToolsCastConvertBase
+ * \brief Base class for all castconverters.
+ */
+
+class ITKToolsCastConvertBase : public itktools::ITKToolsBase
+{
+public:
+  ITKToolsCastConvertBase()
+  {
+    m_InputFileName = "";
+    m_OutputFileName = "";
+    m_UseCompression = false;
+
+    m_InputDirectoryName = "";
+    m_DICOMSeriesUID = "";
+  };
+  ~ITKToolsCastConvertBase(){};
+
+  /** Input parameters */
+  std::string m_InputFileName;
+  std::string m_OutputFileName;
+  bool m_UseCompression;
+
+  /** DICOM specific input parameters. */
+  std::string m_InputDirectoryName;
+  std::string m_DICOMSeriesUID;
+  std::vector<std::string> m_DICOMSeriesRestrictions;
+
+}; // end class ITKToolsCastConvertBase
+
+
+/**
+ * \class ITKToolsCastConvert
+ * \brief Class that castconverts "normal" images.
+ */
+
+template< class TOutputComponentType, unsigned int VDimension >
+class ITKToolsCastConvert : public ITKToolsCastConvertBase
+{
+public:
+  typedef ITKToolsCastConvert Self;
+
+  ITKToolsCastConvert(){};
+  ~ITKToolsCastConvert(){};
+
+  static Self * New( itktools::ComponentType outputComponentType, unsigned int dim )
+  {
+    if ( itktools::IsType<TOutputComponentType>( outputComponentType ) && VDimension == dim )
+    {
+      return new Self;
+    }
+    return 0;
+  }
+
+  void Run(void)
+  {
+    typedef itk::VectorImage<double, VDimension>                InputVectorImageType;
+    typedef itk::VectorImage<TOutputComponentType, VDimension>  OutputVectorImageType;
+
+    typedef itk::Image<double, VDimension>                      InputScalarImageType;
+    typedef itk::Image<TOutputComponentType, VDimension>        OutputScalarImageType;
+
+    typedef typename itk::ImageFileReader< InputVectorImageType >     ImageReaderType;
+    typedef typename itk::ImageFileWriter< OutputVectorImageType >    ImageWriterType;
+
+    typedef typename itk::ShiftScaleImageFilter<
+      InputScalarImageType, OutputScalarImageType >                         ShiftScaleFilterType;
+
+    /** Create and setup the reader. */
+    typename ImageReaderType::Pointer reader = ImageReaderType::New();
+    reader->SetFileName( m_InputFileName.c_str() );
+    reader->Update();
+
+    // Create the disassembler
+    typedef itk::VectorIndexSelectionCastImageFilter<InputVectorImageType, InputScalarImageType> IndexSelectionType;
+    typename IndexSelectionType::Pointer indexSelectionFilter = IndexSelectionType::New();
+    indexSelectionFilter->SetInput(reader->GetOutput());
+
+    // Create the re-assembler
+    typedef itk::ImageToVectorImageFilter<OutputScalarImageType> ImageToVectorImageFilterType;
+    typename ImageToVectorImageFilterType::Pointer imageToVectorImageFilter = ImageToVectorImageFilterType::New();
+
+    // Apply the filter to each channel
+    for(unsigned int channel = 0; channel < reader->GetOutput()->GetNumberOfComponentsPerPixel(); channel++)
+    {
+      // Extract the current channel
+      indexSelectionFilter->SetIndex(channel);
+      indexSelectionFilter->Update();
+
+      // Cast the image
+      //typename RescaleFilterType::Pointer caster = RescaleFilterType::New();
+      typename ShiftScaleFilterType::Pointer caster = ShiftScaleFilterType::New();
+      caster->SetShift( 0.0 );
+      caster->SetScale( 1.0 );
+      caster->SetInput( indexSelectionFilter->GetOutput() );
+      caster->Update();
+
+      // Reassemble the current channel
+      imageToVectorImageFilter->SetNthInput(channel, caster->GetOutput());
+    }
+
+    imageToVectorImageFilter->Update();
+
+    typename ImageWriterType::Pointer writer = ImageWriterType::New();
+    writer->SetFileName(m_OutputFileName);
+    writer->SetInput(imageToVectorImageFilter->GetOutput());
+    writer->Update();
+  }
+
+}; // end class ITKToolsCastConvert
+
+
+/**
+ * \class ITKToolsCastConvertDICOM
+ * \brief Class that castconverts DICOM images.
+ */
+
+template< class TOutputComponentType, unsigned int VDimension >
+class ITKToolsCastConvertDICOM : public ITKToolsCastConvertBase
+{
+public:
+  typedef ITKToolsCastConvertDICOM Self;
+
+  ITKToolsCastConvertDICOM(){};
+  ~ITKToolsCastConvertDICOM(){};
+
+  static Self * New( itktools::ComponentType outputComponentType, unsigned int dim )
+  {
+    if ( itktools::IsType<TOutputComponentType>( outputComponentType ) && VDimension == dim )
+    {
+      return new Self;
+    }
+    return 0;
+  }
+
+  virtual void Run( void )
+  {
+    /** Typedef the correct reader, caster and writer. */
+    typedef itk::Image< double, VDimension >                      InputScalarImageType;
+    typedef itk::Image< TOutputComponentType, VDimension >        OutputScalarImageType;
+
+    typedef typename itk::ImageSeriesReader< InputScalarImageType >   SeriesReaderType;
+    typedef typename itk::CastImageFilter<
+      InputScalarImageType, OutputScalarImageType >                   CastFilterType;
+    typedef typename itk::ImageFileWriter< OutputScalarImageType >    ImageWriterType;
+
+    /** Typedef DICOM stuff. */
+    typedef itk::GDCMImageIO                  GDCMImageIOType;
+    typedef itk::GDCMSeriesFileNames          GDCMNamesGeneratorType;
+    typedef std::vector< std::string >        FileNamesContainerType;
+
+    /** Create the DICOM ImageIO. */
+    typename GDCMImageIOType::Pointer dicomIO = GDCMImageIOType::New();
+
+    /** Get a list of the filenames of the 2D input DICOM images. */
+    GDCMNamesGeneratorType::Pointer nameGenerator = GDCMNamesGeneratorType::New();
+    nameGenerator->SetUseSeriesDetails( true );
+    for ( unsigned int i = 0; i < m_DICOMSeriesRestrictions.size(); ++i )
+    {
+      nameGenerator->AddSeriesRestriction( m_DICOMSeriesRestrictions[ i ] );
+    }
+    nameGenerator->SetInputDirectory( m_InputDirectoryName.c_str() );
+    FileNamesContainerType fileNames = nameGenerator->GetFileNames( m_DICOMSeriesUID );
+
+    /** Create and setup the seriesReader. */
+    typename SeriesReaderType::Pointer seriesReader = SeriesReaderType::New();
+    seriesReader->SetFileNames( fileNames );
+    seriesReader->SetImageIO( dicomIO );
+
+    /** Create and setup caster and writer. */
+    typename CastFilterType::Pointer caster = CastFilterType::New();
+    typename ImageWriterType::Pointer writer = ImageWriterType::New();
+    writer->SetFileName( m_OutputFileName.c_str()  );
+    writer->SetUseCompression( m_UseCompression );
+
+    /** Connect the pipeline. */
+    caster->SetInput(  seriesReader->GetOutput()  );
+    writer->SetInput(  caster->GetOutput()  );
+
+    /**  Do the actual  conversion.  */
+    writer->Update();
+
+    /**  Print  information. */
+    //PrintInfo( seriesReader, writer );
+
+  } // end Run()
+
+}; // end class ITKToolsCastConvertDICOM
+
+
+
+///----------------------------------------
+
+
+
+
+// BELOW IS NOT USED
+
+
+
 
 /** Print image information from the reader and the writer. */
 template< class ReaderType, class WriterType >
@@ -100,205 +310,6 @@ PrintInfo( ReaderType reader, WriterType writer )
   std::cout << std::endl;
 
 }  // end PrintInfo()
-
-
-/** The function that reads the input dicom image and writes the output image.
- * This function is templated over the image types. In the main function
- * we have to make sure to call the right instantiation.
- */
-
-template< class InputImageType, class OutputImageType >
-void
-ReadDicomSeriesCastWriteImage(
-  const std::string & inputDirectoryName,
-  const std::string & outputFileName,
-  const std::string & seriesUID,
-  const std::vector<std::string> & restrictions,
-  const bool & useCompression )
-{
-  /** Typedef the correct reader, caster and writer. */
-  typedef typename itk::ImageSeriesReader< InputImageType >     SeriesReaderType;
-  //typedef typename itk::RescaleIntensityImageFilter<
-    //InputImageType, OutputImageType >                           RescaleFilterType;
-  typedef typename itk::ShiftScaleImageFilter<
-    InputImageType, OutputImageType >                           ShiftScaleFilterType;
-  typedef typename itk::ImageFileWriter< OutputImageType >      ImageWriterType;
-
-  /** Typedef dicom stuff. */
-  typedef itk::GDCMImageIO                  GDCMImageIOType;
-  typedef itk::GDCMSeriesFileNames          GDCMNamesGeneratorType;
-  typedef std::vector< std::string >        FileNamesContainerType;
-
-  /** Create the dicom ImageIO. */
-  typename GDCMImageIOType::Pointer dicomIO = GDCMImageIOType::New();
-
-  /** Get a list of the filenames of the 2D input dicom images. */
-  GDCMNamesGeneratorType::Pointer nameGenerator = GDCMNamesGeneratorType::New();
-  nameGenerator->SetUseSeriesDetails( true );
-  for ( unsigned int i = 0; i < restrictions.size(); ++i )
-  {
-    nameGenerator->AddSeriesRestriction( restrictions[ i ] );
-  }
-  nameGenerator->SetInputDirectory( inputDirectoryName.c_str() );
-  FileNamesContainerType fileNames = nameGenerator->GetFileNames( seriesUID );
-
-  /** Create and setup the seriesReader. */
-  typename SeriesReaderType::Pointer seriesReader = SeriesReaderType::New();
-  seriesReader->SetFileNames( fileNames );
-  seriesReader->SetImageIO( dicomIO );
-
-  /** Create and setup caster and writer. */
-  //typename RescaleFilterType::Pointer caster = RescaleFilterType::New();
-  typename ShiftScaleFilterType::Pointer caster = ShiftScaleFilterType::New();
-  typename ImageWriterType::Pointer  writer = ImageWriterType::New();
-  caster->SetShift( 0.0 );
-  caster->SetScale( 1.0 );
-  writer->SetFileName( outputFileName.c_str()  );
-  writer->SetUseCompression( useCompression );
-
-  /** Connect the pipeline. */
-  caster->SetInput(  seriesReader->GetOutput()  );
-  writer->SetInput(  caster->GetOutput()  );
-
-  /**  Do the actual  conversion.  */
-  writer->Update();
-
-  /**  Print  information. */
-  PrintInfo( seriesReader, writer );
-
-}  // end ReadDicomSeriesCastWriteImage()
-
-
-/** The function that reads the input image and writes the output image.
- * This function is templated over the image types. In the main function
- * we have to make sure to call the right instantiation.
- */
-
-template< class InputImageType, class OutputImageType >
-void
-ReadCastWriteImage(
-  const std::string & inputFileName,
-  const std::string & outputFileName,
-  const bool & useCompression )
-{
-  /**  Typedef the correct reader, caster and writer. */
-  typedef typename itk::ImageFileReader< InputImageType >     ImageReaderType;
-  //typedef typename itk::RescaleIntensityImageFilter<
-    //InputImageType, OutputImageType >                         RescaleFilterType;
-  typedef typename itk::ShiftScaleImageFilter<
-    InputImageType, OutputImageType >                         ShiftScaleFilterType;
-  typedef typename itk::ImageFileWriter< OutputImageType >    ImageWriterType;
-
-  /** Create and setup the reader. */
-  typename ImageReaderType::Pointer reader = ImageReaderType::New();
-  reader->SetFileName( inputFileName.c_str() );
-
-  /** Create and setup caster and writer. */
-  //typename RescaleFilterType::Pointer caster = RescaleFilterType::New();
-  typename ShiftScaleFilterType::Pointer caster = ShiftScaleFilterType::New();
-  typename ImageWriterType::Pointer  writer = ImageWriterType::New();
-  caster->SetShift( 0.0 );
-  caster->SetScale( 1.0 );
-  writer->SetFileName( outputFileName.c_str() );
-  writer->SetUseCompression( useCompression );
-
-  /** Connect the pipeline. */
-  caster->SetInput( reader->GetOutput() );
-  writer->SetInput( caster->GetOutput() );
-
-  /** Do the actual conversion. */
-  writer->Update();
-
-  /** Print information. */
-  PrintInfo( reader, writer );
-
-}  // end ReadWriteImage()
-
-
-/** The function that reads the input vector image and writes the vector output image.
- * This function is templated over the image types. In the main function
- * we have to make sure to call the right instantiation.
- */
-
-template< class InputImageType, class OutputImageType >
-void
-ReadCastWriteVectorImage(
-  const std::string & inputFileName,
-  const std::string & outputFileName,
-  const bool & useCompression )
-{
-  /**  Typedef the correct reader, caster and writer. */
-  typedef typename itk::ImageFileReader< InputImageType >     ImageReaderType;
-  typedef typename itk::VectorCastImageFilter<
-    InputImageType, OutputImageType >                         CasterType;
-  typedef typename itk::ImageFileWriter< OutputImageType >    ImageWriterType;
-
-  /** Create reader, caster and writer. */
-  typename ImageReaderType::Pointer reader = ImageReaderType::New();
-  typename CasterType::Pointer      caster = CasterType::New();
-  typename ImageWriterType::Pointer writer = ImageWriterType::New();
-
-  /** Setup reader, caster and writer. */
-  reader->SetFileName( inputFileName.c_str() );
-  //caster->InPlaceOn();
-  writer->SetFileName( outputFileName.c_str() );
-  writer->SetUseCompression( useCompression );
-
-  /** Connect the pipeline. */
-  caster->SetInput( reader->GetOutput() );
-  writer->SetInput( caster->GetOutput() );
-
-  /** Do the actual conversion. */
-  writer->Update();
-
-  /** Print information. */
-  PrintInfo( reader, writer );
-
-}  // end ReadWriteImage()
-
-
-/** Macros are used in order to make the code in main() look cleaner. */
-
-/** callCorrectReadDicomWriterMacro:
- * A macro to call the dicom-conversion function.
- */
-#define callCorrectReadDicomWriterMacro(typeIn,typeOut) \
-    if ( inputPixelComponentType == #typeIn && outputPixelComponentType == #typeOut ) \
-{ \
-  typedef  itk::Image< typeIn, 3 >    InputImageType; \
-  typedef  itk::Image< typeOut, 3 >  OutputImageType; \
-  ReadDicomSeriesCastWriteImage< InputImageType, OutputImageType >( \
-    inputDirectoryName, outputFileName, seriesUID, restrictions, useCompression ); \
-}
-
-/** callCorrectReadWriterMacro:
- * A macro to call the conversion function.
- */
-#define callCorrectReadWriterMacro(typeIn,typeOut,dim) \
-    if ( inputPixelComponentType == #typeIn \
-    && outputPixelComponentType == #typeOut && inputDimension == dim ) \
-{ \
-  typedef  itk::Image< typeIn, dim >    InputImageType; \
-  typedef  itk::Image< typeOut, dim >  OutputImageType; \
-  ReadCastWriteImage< InputImageType, OutputImageType >( \
-    inputFileName, outputFileName, useCompression ); \
-}
-
-/** callCorrectReadWriterMultiComponentMacro:
- * A macro to call the conversion function for multi-component images.
- */
-#define callCorrectReadWriterMultiComponentMacro( typeIn, typeOut, vecDim, dim ) \
-    if ( inputPixelComponentType == #typeIn \
-      && outputPixelComponentType == #typeOut \
-      && numberOfComponents == vecDim && inputDimension == dim ) \
-{ \
-  typedef itk::Vector< typeIn, vecDim >        InputVectorType; \
-  typedef itk::Vector< typeOut, vecDim >       OutputVectorType; \
-  typedef itk::Image< InputVectorType, dim >   InputImageType; \
-  typedef itk::Image< OutputVectorType, dim >  OutputImageType; \
-  ReadCastWriteVectorImage< InputImageType, OutputImageType >( \
-    inputFileName, outputFileName, useCompression ); \
-}
 
 
 #endif //__castconverthelpers_h__
