71245886e1b34e4736007d345fcb0e608922fd02
Stefan Klein <s.klein@erasmusmc.nl>
2012-02-03 18:48:47 +0100

    -ENH: added 4D support for pxcastconvert. -DOC: restructured help message, and removed some implementation details.

--- src/castconvert/castconvert.cxx
+++ src/castconvert/castconvert.cxx
@@ -52,31 +52,27 @@ std::string GetHelpString( void )
 {
   std::stringstream ss;
   ss << "Description:\n"
+     << "This program converts between many image formats.\n"
      << "This is done by reading in an image, possibly casting of the image,\n"
-     << "and subsequently writing the image to some format.\n"
-     << "With converting we mean changing the extension of the image,\n"
-     << "such as bmp, mhd, etc. With casting we mean changing the component\n"
-     << "type of a voxel, such as short, unsigned long, float.\n"
-     << "Casting is currently done for scalar images using the ShiftScaleImageFilter,\n"
-     << "where values are mapped to itself, leaving the intensity range\n"
-     << "the same. NOTE that when casting to a component type with a\n"
-     << "smaller dynamic range, information might get lost. In this case\n"
-     << "we might use the RescaleIntensityImageFilter to linearly\n"
-     << "rescale the image values. For multi-component images, such as vector\n"
-     << "or RGB images, casting is done using the itk::VectorCastImageFilter.\n"
-     << "Currently supported are the SCALAR pixel types and also multi-\n"
-     << "component pixel types, such as vector and RGB pixels. For multi-\n"
-     << "component pixel types, everything is read in as an itk::Vector with\n"
-     << "the correct pixel component type and number of components. This is\n"
-     << "also the case for the writer.\n"
-     << "Input images can be in all file formats ITK supports and for which\n"
-     << "the ImageFileReader works, and additionally 3D dicom series\n"
-     << "using the ImageSeriesReader. It is also possible to extract a specific\n"
-     << "DICOM series from a directory by supplying the seriesUID. The pixel\n"
-     << "component type should of course be a component type supported by the\n"
-     << "file format. Output images can be in all file formats ITK supports and\n"
-     << "for which the ImageFileReader works, so no dicom output is\n"
-     << "currently supported.\n" << std::endl
+     << "and subsequently writing the image to the user-specified format.\n"
+     << "\nDefinitions:\n"
+     << "- converting: changing the extension of the image, e.g. bmp, mhd, etc.\n"
+     << "- casting: changing the component type of a voxel, e.g. short, float,\n"
+     << "           unsigned long, etc.\n"
+     << "\nNotes:\n"
+     << "- Casting of scalar images is done by the itk::ShiftScaleImageFilter,\n"
+     << "  where values are mapped to itself, leaving the intensity range\n"
+     << "  the same. NB: When casting to a component type with smaller dynamic\n"
+     << "  range, information might get lost.\n"
+     << "- Casting of multi-component images, such as vector or RGB images, is\n"
+     << "  done using the itk::VectorCastImageFilter.\n"
+     << "- Input images can be in all file formats ITK supports and for which\n"
+     << "  the itk::ImageFileReader works, and additionally 3D dicom series.\n"
+     << "  It is also possible to extract a specific DICOM series from a directory\n"
+     << "  by supplying the seriesUID.\n"
+     << "- Output images can be in all file formats ITK supports and for which\n"
+     << "  the itk::ImageFileWriter works. Dicom output is not supported yet.\n"
+     << "\n" << std::endl
      << "Usage:\n"
      << "pxcastconvert\n"
      << "  -in      inputfilename\n"
@@ -93,8 +89,8 @@ std::string GetHelpString( void )
      << "  [-z]     compression flag; if provided, the output image is compressed\n\n"
      << "OutputPixelComponentType should be one of {[unsigned_]char, [unsigned_]short,\n"
      << "  [unsigned_]int, [unsigned_]long, float, double}.\n"
-     << "OutputPixelComponentType should additionally be supported by the output file format.\n"
-     << "The compression flag \"-z\" may be ignored by some output image formats." << std::endl;
+     << "NB: Not every image format supports all OutputPixelComponentTypes.\n"
+     << "NB2: Not every image format supports the compression flag \"-z\"." << std::endl;
 
   return ss.str();
 
@@ -106,6 +102,9 @@ extern void ITKToolsCastConvert2D(
   itktools::ComponentType outputComponentType, unsigned int dim,
   ITKToolsCastConvertBase * & castConvert );
 extern void ITKToolsCastConvert3D(
+  itktools::ComponentType outputComponentType, unsigned int dim,
+  ITKToolsCastConvertBase * & castConvert );
+extern void ITKToolsCastConvert4D(
   itktools::ComponentType outputComponentType, unsigned int dim,
   ITKToolsCastConvertBase * & castConvert );
 extern void ITKToolsCastConvertDICOM3D(
@@ -235,6 +234,11 @@ int main( int argc, char **argv )
 #ifdef ITKTOOLS_3D_SUPPORT
       if ( !castConvert ) ITKToolsCastConvert3D( componentType, dim, castConvert );
 #endif
+
+#ifdef ITKTOOLS_4D_SUPPORT
+      if ( !castConvert ) ITKToolsCastConvert4D( componentType, dim, castConvert );
+#endif
+
     }
     else
     {
