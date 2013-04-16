144b133ce2f6edb721659860cdf39022bfed46f2
Marius Staring <m.staring@lumc.nl>
2013-04-16 13:20:23 +0200

    ENH: add a puthon script for image compression

---
+++ src/scripts/pxcompressimage.py
@@ -0,0 +1,71 @@
+import fileinput
+import sys
+import glob
+import os.path
+from optparse import OptionParser
+
+#import os
+#import shutil
+#import re
+
+#from optparse import OptionParser # Deprecated with python 2.7
+#import argparse # Requires python 2.7 or greater
+
+#-------------------------------------------------------------------------------
+#  This script ...
+#
+
+allowable = [ '.mhd', '.mha', '.gipl', '.tif', '.tiff', '.jpg' ];
+
+#-------------------------------------------------------------------------------
+# the main function
+def main() :
+
+  # usage, parse parameters
+  usage = "usage: %prog [options] arg"
+  parser = OptionParser( usage );
+
+  # options
+  parser.add_option( "-t", "--opct", dest="opct", help="output (component) type" );
+  (options, args) = parser.parse_args();
+
+  # Get all arguments
+  arguments = sys.argv[1:];
+
+  # Get optional argument output type
+  opct = "";
+  if options.opct : opct = " -opct " + options.opct;
+
+  # Construct a list of file names
+  # all arguments may contain wild cards
+  fileNameList = [];
+  for argument in arguments :
+    fileNameList.extend( glob.glob( argument ) );
+
+  # Process the list
+  for fileName in fileNameList :
+
+    # Check if file exists
+    # Probably not needed due to glob:
+    if not os.path.exists( fileName ) :
+      continue;
+
+    # Check if file is not binary, is readable and writable
+
+    # Check if file extension is allowable
+    ( root, extension ) = os.path.splitext( fileName );
+    if extension not in allowable :
+      print "Skipping " + fileName;
+      continue;
+    else :
+      print "Processing " + fileName + " ...";
+
+    # Compress image
+    command = "pxcastconvert -in " + fileName + " -out " + fileName + opct + " -z >> /dev/null";
+    os.system( command );
+
+  return 0
+
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+  sys.exit(main())
